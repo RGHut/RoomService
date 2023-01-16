542ab5292133ec221238a2abcc6f2e9a7f2051af
Roland Hut <hutroland50@gmail.com>
2023-01-16 14:43:00 +0100

    split controllers, added more functionality.

--- src/main/java/CG/RoomService/Controllers/BookingController.java
+++ src/main/java/CG/RoomService/Controllers/BookingController.java
@@ -1,18 +1,57 @@
 package CG.RoomService.Controllers;
 
+import CG.RoomService.Models.Booking;
 import CG.RoomService.Models.Building;
+import CG.RoomService.Models.Room;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @RestController
 public class BookingController {
 
-    private final ArrayList<Building> buildings = new ArrayList<Building>();
+    private final ArrayList<Booking> bookings = new ArrayList<Booking>();
 
-    public void addBuilding() {
-        Building building = new Building("Corpus den Hoorn 106");
-        building.addRoom("grote vergader ruimte", 1, 8, true);
-        buildings.add(building);
+    @GetMapping("/bookings")
+    public ArrayList<Booking> getBookings() {
+        return bookings;
     }
+
+    @PostMapping("/makeBooking")
+    public ResponseEntity<?> makeBooking(@RequestParam String buildingName, @RequestParam String roomName, @RequestParam LocalDateTime time) {
+        for (Building building: BuildingController.getBuildings()) {
+            if (building.getName().equals(buildingName)) {
+                for (Room room : building.getRooms()) {
+                    if (room.getName().equals(roomName)) {
+                        if (isBooked(room, time)) {
+                            return ResponseEntity.status(400).body("{\"error\":\"Room is already booked!\"" + "}");
+                        } else {
+                            bookings.add(new Booking(room, time));
+                            return ResponseEntity.status(200).body("{\"Booking created\"}");
+                        }
+                    }
+                }
+                return ResponseEntity.status(400).body("{\"error\":" + "\"Room does not exists!\"" + "}");
+            }
+        }
+        return ResponseEntity.status(400).body("{\"error\":" + "\"Building does not exists!\"" + "}");
+    }
+
+    private boolean isBooked(Room room, LocalDateTime time) {
+        boolean booked = false;
+        for (Booking booking : bookings) {
+            if (booking.getRoom().equals(room)) {
+                if (booking.getTimeStart().equals(time)) {
+                    booked = true;
+                }
+            }
+        }
+        return booked;
+    }
+
 }
---
+++ src/main/java/CG/RoomService/Controllers/BuildingController.java
@@ -0,0 +1,70 @@
+package CG.RoomService.Controllers;
+import CG.RoomService.Models.Building;
+import CG.RoomService.Models.Room;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+
+@RestController
+public class BuildingController {
+
+    private static final ArrayList<Building> buildings = new ArrayList<Building>();
+
+
+    @GetMapping("/buildings")
+    public static ArrayList<Building> getBuildings() {
+        return buildings;
+    }
+
+    @PostMapping("/getRooms")
+    public ResponseEntity<?> getRooms(@RequestParam String name) {
+        for (Building building: buildings) {
+            if (building.getName().equals(name)) {
+                return ResponseEntity.status(200).body(building.getRooms());
+            }
+        }
+        return ResponseEntity.status(400).body("{\"error\":" + "\"Building does not exists!\"" + "}");
+    }
+
+    @PostMapping("/addBuilding")
+    public ResponseEntity<?> addBuilding(@RequestParam String name) {
+
+        for (Building building: buildings) {
+            if (building.getName().equals(name)) {
+                return ResponseEntity.status(400).body("{\"error\":" + "\"Building already exists!\"" + "}");
+            }
+        }
+        Building building = new Building(name);
+        buildings.add(building);
+        return ResponseEntity.status(200).body("{\"created\":\"" + name + "\"}");
+
+    }
+
+    @PostMapping("/addRoom")
+    public ResponseEntity<?> addRoom(@RequestParam String buildingName, @RequestParam String roomName) {
+//        v placeholder parameters
+        int floor = 0;
+        int maxOccupancy= 8;
+        boolean accessible = true;
+//        ^ placeholder parameters
+        for (Building building: buildings) {
+            if (building.getName().equals(buildingName)) {
+                for (Room room: building.getRooms()) {
+                    if (room.getName().equals(roomName)) {
+                        return ResponseEntity.status(400).body("{\"error\":" + "\"Room already exists!\"" + "}");
+                    }
+                }
+                building.addRoom(roomName, floor, maxOccupancy, accessible);
+                return ResponseEntity.status(200).body("{\"created\":\"" + roomName + "\"}");
+            } else {
+                return ResponseEntity.status(400).body("{\"error\":" + "\"Building does not exists!\"" + "}");
+            }
+        }
+        return ResponseEntity.status(200).body("{\"error\":" + "\"Unexpected error!\"" + "}");
+    }
+}
