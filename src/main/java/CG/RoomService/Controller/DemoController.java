5593a81dc47a1e93af3c1d89331194cb5f79c16d
Tom Sixma <sixmatom@gmail.com>
2023-01-17 13:55:47 +0100

    adding jwt to login

---
+++ src/main/java/CG/RoomService/Controller/DemoController.java
@@ -0,0 +1,15 @@
+package CG.RoomService.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo-controller")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello i'm secured");
+    }
+}
