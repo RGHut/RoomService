f61c24c3b18a5798f366f18bdf6e0aa2cc105780
Tom Sixma <sixmatom@gmail.com>
2023-03-09 13:53:25 +0100

    implemented httpresponse

--- src/main/java/CG/RoomService/Controllers/AuthenticationController.java
+++ src/main/java/CG/RoomService/Controllers/AuthenticationController.java
@@ -36,180 +36,9 @@ import java.util.Locale;
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
-public class AuthenticationController {
-    HttpServletResponse response = new HttpServletResponse() {
-        @Override
-        public String getCharacterEncoding() {
-            return null;
-        }
+public class AuthenticationController implements HttpServletResponse {
 
-        @Override
-        public String getContentType() {
-            return null;
-        }
-
-        @Override
-        public ServletOutputStream getOutputStream() throws IOException {
-            return null;
-        }
-
-        @Override
-        public PrintWriter getWriter() throws IOException {
-            return null;
-        }
-
-        @Override
-        public void setCharacterEncoding(String charset) {
-
-        }
-
-        @Override
-        public void setContentLength(int len) {
-
-        }
-
-        @Override
-        public void setContentLengthLong(long length) {
-
-        }
-
-        @Override
-        public void setContentType(String type) {
-
-        }
-
-        @Override
-        public void setBufferSize(int size) {
-
-        }
-
-        @Override
-        public int getBufferSize() {
-            return 0;
-        }
-
-        @Override
-        public void flushBuffer() throws IOException {
-
-        }
-
-        @Override
-        public void resetBuffer() {
-
-        }
-
-        @Override
-        public boolean isCommitted() {
-            return false;
-        }
-
-        @Override
-        public void reset() {
-
-        }
-
-        @Override
-        public void setLocale(Locale loc) {
-
-        }
-
-        @Override
-        public Locale getLocale() {
-            return null;
-        }
-
-        @Override
-        public void addCookie(Cookie cookie) {
-
-        }
-
-        @Override
-        public boolean containsHeader(String name) {
-            return false;
-        }
-
-        @Override
-        public String encodeURL(String url) {
-            return null;
-        }
-
-        @Override
-        public String encodeRedirectURL(String url) {
-            return null;
-        }
-
-        @Override
-        public void sendError(int sc, String msg) throws IOException {
-
-        }
-
-        @Override
-        public void sendError(int sc) throws IOException {
-
-        }
-
-        @Override
-        public void sendRedirect(String location) throws IOException {
-
-        }
-
-        @Override
-        public void setDateHeader(String name, long date) {
-
-        }
-
-        @Override
-        public void addDateHeader(String name, long date) {
-
-        }
-
-        @Override
-        public void setHeader(String name, String value) {
-
-        }
-
-        @Override
-        public void addHeader(String name, String value) {
-
-        }
-
-        @Override
-        public void setIntHeader(String name, int value) {
-
-        }
-
-        @Override
-        public void addIntHeader(String name, int value) {
-
-        }
-
-        @Override
-        public void setStatus(int sc) {
-
-        }
-
-        @Override
-        public int getStatus() {
-            return 0;
-        }
-
-        @Override
-        public String getHeader(String name) {
-            return null;
-        }
-
-        @Override
-        public Collection<String> getHeaders(String name) {
-            return null;
-        }
-
-        @Override
-        public Collection<String> getHeaderNames() {
-            return null;
-        }
-    };
-
-
+    private final HttpServletResponse response;
     private final BuildingService buildingService;
     private final BookingService bookingService;
     private final UserService userService;
@@ -302,4 +131,173 @@ public class AuthenticationController {
         return ResponseEntity.ok("Hello i'm a secured Werk");
     }
 
+    @Override
+    public void addCookie(Cookie cookie) {
+
+    }
+
+    @Override
+    public boolean containsHeader(String name) {
+        return false;
+    }
+
+    @Override
+    public String encodeURL(String url) {
+        return null;
+    }
+
+    @Override
+    public String encodeRedirectURL(String url) {
+        return null;
+    }
+
+    @Override
+    public void sendError(int sc, String msg) throws IOException {
+
+    }
+
+    @Override
+    public void sendError(int sc) throws IOException {
+
+    }
+
+    @Override
+    public void sendRedirect(String location) throws IOException {
+
+    }
+
+    @Override
+    public void setDateHeader(String name, long date) {
+
+    }
+
+    @Override
+    public void addDateHeader(String name, long date) {
+
+    }
+
+    @Override
+    public void setHeader(String name, String value) {
+
+    }
+
+    @Override
+    public void addHeader(String name, String value) {
+
+    }
+
+    @Override
+    public void setIntHeader(String name, int value) {
+
+    }
+
+    @Override
+    public void addIntHeader(String name, int value) {
+
+    }
+
+    @Override
+    public void setStatus(int sc) {
+
+    }
+
+    @Override
+    public int getStatus() {
+        return 0;
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaders(String name) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        return null;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return null;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String charset) {
+
+    }
+
+    @Override
+    public void setContentLength(int len) {
+
+    }
+
+    @Override
+    public void setContentLengthLong(long length) {
+
+    }
+
+    @Override
+    public void setContentType(String type) {
+
+    }
+
+    @Override
+    public void setBufferSize(int size) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale loc) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
 }
