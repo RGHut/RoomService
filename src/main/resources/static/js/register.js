39388af265aab09f0c3b0b9a7d7eea1163f2bea5
Tom Sixma <sixmatom@gmail.com>
2023-03-16 10:06:52 +0100

    added register js

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#register-form").submit(function (e) {
+        e.preventDefault();
+
+        var firstName = $("firstName").val();
+        var lastName = $("lastName").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+        var company = $("company").val();
+
+        $.ajax({
+            url: "http://localhost:8080/test/authenticate",
+            type: "POST",
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password,
+                company: company,
+            },
+            success: function (data) {
+                var token = data.token;
+                
+
+                // Store the token in local storage
+                localStorage.setItem("jwtToken", token);
+                window.location.href = "../static/home.html";
+            },
+            error: function (xhr, status, error) {
+                
+                console.log("Error: " + xhr.responseJSON.error );
+            }
+        });
+    });
+});
